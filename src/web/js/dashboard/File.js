960b57ca7dfccd0aa9434dfdb87156afd6f4247e
dlhsiao <danalhsiao@gmail.com>
2019-05-06 18:59:15 -0400

    Add if statements for WEB vs APP

--- src/web/js/dashboard/File.js
+++ src/web/js/dashboard/File.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 
 class File extends Component {
   render = () => {
-    return (
-        <div className='file-wrapper'>
-          <div className="file" onClick={this.handleFileClick}>
-            <img src='/img/pyret-logo.png'/>
-            <p className='truncate'>{this.props.name}</p>
+    if (MODE == "WEB"){
+      return (
+          <div className='file-wrapper'>
+            <div className="file" onClick={this.handleFileClick}>
+              <img src='/img/pyret-logo.png'/>
+              <p className='truncate'>{this.props.name}</p>
+            </div>
           </div>
-        </div>
-    );
+      );
+    }
+    else if (MODE == "APP"){
+      return (
+          <div className='file-wrapper'>
+            <div className="file" onClick={this.handleFileClick}>
+              <img src='../img/pyret-logo.png'/>
+              <p className='truncate'>{this.props.name}</p>
+            </div>
+          </div>
+      );
+    }
   }
 
   handleFileClick = () => {
